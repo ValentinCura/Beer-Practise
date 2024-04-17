caa1fd8897d05098104010f3a4ab1411c360dabc
Valentin Cura <valencuran@gmail.com>
2024-04-17 14:45:29 -0300

    Available Commit

--- src/components/available/Available.jsx
+++ src/components/available/Available.jsx
@@ -1,78 +1,48 @@
 import React, { useState } from 'react';
-import { beers as initialBeers } from '../data/Data';
+import { beers } from '../data/Data';
 import { Button, Card, ListGroup } from 'react-bootstrap';
 import Count from '../count/Count';
-import NewBeer from '../newBeer/NewBeer';
 
 const Available = () => {
-  const conversion = 1000;
-  const [beers, setBeers] = useState(initialBeers);
-  const [filteredBeers, setFilteredBeers] = useState(false);
-  const [showNewBeerForm, setShowNewBeerForm] = useState(false); // Estado para controlar la visibilidad del formulario
+    const conversion = 1000;
 
-  const handleAvailableButton = () => {
-    setFilteredBeers(!filteredBeers);
-  };
+    const [filteredBeers, setFilteredBeers] = useState(false);
 
-  const handleNewBeerSubmit = (formData) => {
-    const newBeer = {
-      id: beers.length + 9, // Genera un ID único a partir de 9
-      beerName: formData.beerName,
-      beerStyle: formData.beerStyle,
-      price: formData.price,
-      available: formData.available,
-      beerImgUrl: formData.imageUrl,
+    const handleAvailableButton = () => {
+        setFilteredBeers(!filteredBeers);
     };
 
-    setBeers([...beers, newBeer]);
-    console.log('Nueva cerveza agregada:', newBeer);
+    const availableBeers = filteredBeers ? beers.filter(beer => beer.available) : beers;
 
-    // Oculta el formulario después de agregar la cerveza
-    setShowNewBeerForm(false);
-  };
-
-  return (
-    <div>
-      <h1 className="text-black">Cervezas: </h1>
-      <div className="d-flex flex-wrap">
-        {beers.map(beer => (
-          <Card key={beer.id} className='m-1 d-flex flex-row' style={{ width: '380px' }}>
-            <Card.Img
-              height={170}
-              variant="top"
-              src={beer.beerImgUrl}
-            />
-            <Card.Body>
-              <Card.Title>{beer.beerName}</Card.Title>
-              <ListGroup className="list-group-flush">
-                <ListGroup.Item>{beer.beerStyle}</ListGroup.Item>
-                <ListGroup.Item>{beer.price * conversion}</ListGroup.Item>
-                <ListGroup.Item>{beer.available ? 'Disponible' : 'No disponible'}</ListGroup.Item>
-              </ListGroup>
-            </Card.Body>
-          </Card>
-        ))}
-      </div>
-
-      <Button className="btn btn-secondary m-3" onClick={handleAvailableButton}>
-        {filteredBeers ? 'Mostrar Todas' : 'Filtrar Disponibles'}
-      </Button>
-      <Count beers={beers} />
-
-      {/* Botón para mostrar/ocultar el formulario de agregar cerveza */}
-      <Button className="btn btn-primary m-3" onClick={() => setShowNewBeerForm(!showNewBeerForm)}>
-        {showNewBeerForm ? 'Cancelar' : 'Agregar Cerveza'}
-      </Button>
-
-      {/* Renderiza el formulario NewBeer si showNewBeerForm es verdadero */}
-      {showNewBeerForm && (
+    return (
         <div>
-          <h1>Formulario Nueva Cerveza</h1>
-          <NewBeer onSubmit={handleNewBeerSubmit} />
+            <h1 className="text-light">Cervezas: </h1>
+            <div className="d-flex flex-wrap">
+                {availableBeers.map(beer => (
+                    <Card key={beer.id} className='m-1 d-flex flex-row' style={{ width: '380px' }}>
+                        <Card.Img
+                            height={170}
+                            variant="top"
+                            src={beer.beerImgUrl}>
+                        </Card.Img>
+                        <Card.Body >
+                            <Card.Title>{beer.beerName}</Card.Title>
+                            <ListGroup className="list-group-flush">
+                                <ListGroup.Item>{beer.beerStyle}</ListGroup.Item>
+                                <ListGroup.Item>{beer.price * conversion}</ListGroup.Item>
+                                <ListGroup.Item>{beer.available ? 'Disponible' : 'No disponible'}</ListGroup.Item>
+                            </ListGroup>
+                        </Card.Body>
+                    </Card>
+                ))}
+            </div>
+
+            <Button className="btn btn-secondary m-3" onClick={handleAvailableButton}>Filtrar solo disponible</Button>
+            <Count beers={availableBeers} />
+
         </div>
-      )}
-    </div>
-  );
-};
+
+    );
+}
 
 export default Available;
